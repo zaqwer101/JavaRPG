f8e088207856479324e7f8ab67dbdc0baa8680f8
zaqwer101 <platonov256@gmail.com>
2019-09-01 02:06:51 +0800

    Position дописан

--- src/main/java/Game/Position.java
+++ src/main/java/Game/Position.java
@@ -4,5 +4,34 @@ package Game;
  * Позиция объекта в игровой локации
  */
 public class Position {
-    // TODO
+    private int x, y;
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void add(Position position)
+    {
+        this.x += position.x;
+        this.y += position.y;
+    }
+
+    public void sub(Position position)
+    {
+        this.x -= position.x;
+        this.y -= position.y;
+    }
+
+    public int getDistance(Position position)
+    {
+        return (int) Math.sqrt(
+                Math.pow(this.x - position.x, 2) + Math.pow(this.y - position.y, 2)
+        );
+    }
 }
