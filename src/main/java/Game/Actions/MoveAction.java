2a24e06aa79f2c29d334c4073d57edb7ea51a318
zaqwer101 <platonov256@gmail.com>
2019-10-13 22:18:55 +0800

    Начал перепиливать передвижение существ

--- src/main/java/Game/Actions/MoveAction.java
+++ src/main/java/Game/Actions/MoveAction.java
@@ -16,16 +16,6 @@ public class MoveAction extends Action
     @Override
     public void use()
     {
-        try
-        {
-            user.teleport(new Position(
-                    user.getPosition().getX() + direction[0],
-                    user.getPosition().getY() + direction[1]
-            ));
-        }
-        catch (Exception e)
-        {
-            JavaRPG.log(e.getMessage());
-        }
+        // TODO
     }
 }
