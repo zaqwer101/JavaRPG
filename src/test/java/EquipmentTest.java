1a13e01a4078604ab3d229306d1ce3336ff51507
zaqwer101 <platonov256@gmail.com>
2020-02-12 18:49:55 +0800

    Test: EquipmentTest.equipmentRequirementsTest

--- src/test/java/EquipmentTest.java
+++ src/test/java/EquipmentTest.java
@@ -1,5 +1,6 @@
 import Game.Core.*;
 import Game.Effects.InstantDamage;
+import Game.Effects.PeriodicStatsEffect;
 import Game.Items.Equipment.Armor.BodyArmor;
 import Game.Items.Equipment.Equipment;
 import org.junit.Before;
@@ -59,7 +60,6 @@ public class EquipmentTest
         resists.setResist(Resists.DamageType.PHYSICAL, 90);
         equipment = new BodyArmor("Доспех", 10, 1, resists, equipmentStats, equipmentRequirements);
         location.getPosition(dummy.getPosition()).addItem(equipment); // кладём предмет в локацию, чтобы можно было надеть оттуда
-        // Creature tester = new Creature("Dummy", '!', new Position(1, 1), location);
 
         assertEquals(8, dummy.getHp()[0]);
 
@@ -67,5 +67,35 @@ public class EquipmentTest
         new InstantDamage("ХУЯК", Resists.DamageType.PHYSICAL, 10).apply(dummy);
 
         assertEquals(7, dummy.getHp()[0]);
+    }
+
+    @Test
+    public void equipmentRequirementsTest()
+    {
+        equipmentRequirements.setStat("strength", 100);
+        equipmentRequirements.setStat("intelligence", 100);
+        equipment = new BodyArmor("Доспех", 10, 1, null, null, equipmentRequirements);
+        dummy.addToInventory(equipment);
+        assertFalse(dummy.equip(equipment));
+        assertNull(dummy.getEquipment(Equipment.EquipmentSlot.EQUIPMENT_BODY));
+
+        Stats buffStats = new Stats();
+        buffStats.setStat("strength", 110);
+        buffStats.setStat("intelligence", 110);
+        dummy.addEffect(new PeriodicStatsEffect(1, "Чтоб броня не спадала", buffStats));
+        dummy.endTurn();
+
+        assertTrue(dummy.equip(equipment));
+        assertEquals(equipment, dummy.getEquipment(Equipment.EquipmentSlot.EQUIPMENT_BODY));
+
+        assertEquals(1, dummy.getEffects()[0].getDuration());
+        dummy.endTurn();
+        assertEquals(0, dummy.getEffects().length);
+
+        assertEquals(1, dummy.getStat(  "strength"      ));
+        assertEquals(1, dummy.getStat(  "intelligence"  ));
+
+        assertNull(dummy.getEquipment(Equipment.EquipmentSlot.EQUIPMENT_BODY)); // Броня должна упасть
+
     }
 }
